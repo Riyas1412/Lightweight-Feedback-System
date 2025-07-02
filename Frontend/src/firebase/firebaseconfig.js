405399b59a3f7b9d6d4c3468b6595fcbeb1a37cb
Nalla Mohamed Riyas <ajriyas00723@gmail.com>
2025-07-02 10:18:24 +0530

    hide firebaseconfig.js

--- Frontend/src/firebase/firebaseconfig.js
+++ Frontend/src/firebase/firebaseconfig.js
@@ -4,13 +4,13 @@ import { getAuth } from "firebase/auth";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBmDteoc5cZNd926OFZdg-ZRyqvCJcMtNA",
-  authDomain: "feedback-app-414af.firebaseapp.com",
-  projectId: "feedback-app-414af",
-  storageBucket: "feedback-app-414af.firebasestorage.app",
-  messagingSenderId: "220037378686",
-  appId: "1:220037378686:web:f43d0fc2228daebada1e2f",
-  measurementId: "G-9QR74LS7SX"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
